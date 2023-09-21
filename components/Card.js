6444b2242ba15fea990ef53b84a52e246d641906
Fei <ozzyfly@yahoo.com.tw>
2023-09-21 16:49:01 -0700

    add Card.js

---
+++ components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Card = (props) => {
+  return <View style={styles.card}>{props.children}</View>;
+};
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 10,
+    elevation: 5,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: 'white',
+    padding: 20,
+    margin: 10,
+  },
+});
+
+export default Card;
