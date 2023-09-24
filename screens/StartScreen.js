98b3f174f9899a541c7d9ddbf675f008ca54006c
Fei <ozzyfly@yahoo.com.tw>
2023-09-24 14:46:18 -0700

    make the startscreen code more concise

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, Text, Button, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import Card from '../components/Card';
 import CustomInput from '../components/CustomInput'; 
 import CustomButton from '../components/CustomButton'; 
 
-
-const StartScreen = ({ onStart, onReset, userData }) => {
-  const [name, setName] = useState(userData.name);
-  const [email, setEmail] = useState(userData.email);
-  const [phoneNumber, setPhoneNumber] = useState(userData.phoneNumber);
+const StartScreen = ({ onStart, onReset, userData = { name: '', email: '', phoneNumber: '' } }) => {
+  const [formData, setFormData] = useState(userData);
   const [checkboxSelected, setCheckboxSelected] = useState(false);
   const [errors, setErrors] = useState({ name: '', email: '', phoneNumber: '' });
+
+  const { name, email, phoneNumber } = formData;
 
   const validateInput = () => {
     let isValid = true;
@@ -36,27 +35,24 @@ const StartScreen = ({ onStart, onReset, userData }) => {
     return isValid;
   };
 
-  const handleReset = () => {
-    setName('');
-    setEmail('');
-    setPhoneNumber('');
+  const handleReset = useCallback(() => {
+    setFormData({ name: '', email: '', phoneNumber: '' });
     setCheckboxSelected(false);
     setErrors({ name: '', email: '', phoneNumber: '' });
-  };
+  }, []);
 
-  const handleStart = () => {
+  const handleStart = useCallback(() => {
     if (validateInput()) {
-      const userData = { name, email, phoneNumber };
-      onStart(userData);
+      onStart(formData);
     }
-  };
+  }, [formData, onStart]);
 
   return (
-    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.screen}>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.screen}>
       <Card>
-        <CustomInput label="Name" value={name} onChangeText={setName} error={errors.name} />
-        <CustomInput label="Email" value={email} onChangeText={setEmail} error={errors.email} />
-        <CustomInput label="Phone Number" value={phoneNumber} onChangeText={setPhoneNumber} error={errors.phoneNumber} />
+        <CustomInput label="Name" value={name} onChangeText={(text) => setFormData(prev => ({ ...prev, name: text }))} error={errors.name} />
+        <CustomInput label="Email" value={email} onChangeText={(text) => setFormData(prev => ({ ...prev, email: text }))} error={errors.email} />
+        <CustomInput label="Phone Number" value={phoneNumber} onChangeText={(text) => setFormData(prev => ({ ...prev, phoneNumber: text }))} error={errors.phoneNumber} />
 
         <View style={styles.checkboxContainer}>
           <Button title="I am not a robot" onPress={() => setCheckboxSelected(!checkboxSelected)} />
@@ -74,17 +70,6 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    // alignItems: 'center',
-  },
-  input: {
-    width: '80%',
-    borderBottomWidth: 1,
-    marginBottom: 20,
-    padding: 10,
-  },
-  errorText: {
-    color: 'red',
-    marginBottom: 10,
   },
   checkboxContainer: {
     flexDirection: 'row',
